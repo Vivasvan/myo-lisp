787d733ffd5816c491a31e352e9adc0c0d9eb361
Viv Paranjape <vivasvanp@gmail.com>
2017-09-17 09:13:42 +0100

    initial commit, first chapter + ex

---
+++ first.hs
@@ -0,0 +1,10 @@
+module Main where
+import System.Environment
+
+main :: IO ()
+main = do 
+  args <- getArgs
+  let x = read(args !! 0) :: Float
+  let y = read(args !! 1) :: Float
+  
+  putStrLn $ show $ x + y
